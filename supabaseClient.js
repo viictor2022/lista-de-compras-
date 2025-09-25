d49bb094bbc8ce90166d0be37a4dbcc019229495
viictor2022 <159586358+viictor2022@users.noreply.github.com>
2025-09-25 20:49:44 -0300

    zfdhdhgnd

---
+++ supabaseClient.js
@@ -0,0 +1,8 @@
+// Importa a função de criação do client Supabase via CDN
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+// Cria o client com a URL e a chave fornecidas
+export const supabase = createClient(
+  'https://rtueweqqtlkuvibhwvrf.supabase.co
+  'XZpYmh3dnJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NjExNDIsImV4cCI6MjA2NDEzNzE0Mn0.4ixV2swN_8cjU8nkfZUHgTlkXuEvOt-RPAHeNEaGywIeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0dWV3ZXFxdGxrd'
+)''
